32cf39809f1d1111ecc51da288faabe5c45c6eaa
Antonio Prado <antonioeprado18@gmail.com>
2022-11-18 14:46:16 -0300

    chore: code adjusted for deploy on render

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(authRoute);
 app.use(expensesRoute);
 
-app.listen(5000, () => {
-	console.log("Server running on port: 5000");
+app.listen(process.env.PORT, () => {
+	console.log("Server running on port: ", process.env.PORT);
 });
